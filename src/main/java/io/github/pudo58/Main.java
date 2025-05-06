aad4cb2c467e135c297df12fd234f1d84bf157a8
Lã Văn Thọ <tholv@dc1.gemvietnam.com>
2025-05-06 13:02:50 +0700

    demo

--- src/main/java/io/github/pudo58/Main.java
+++ src/main/java/io/github/pudo58/Main.java
@@ -2,12 +2,14 @@ package io.github.pudo58;
 
 import io.github.pudo58.bot.introduction.BadWordFilter;
 import io.github.pudo58.bot.introduction.CommandListener;
+import io.github.pudo58.constant.BotConstant;
+import io.github.pudo58.utils.EnvUtils;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 
 public class Main {
     public static void main(String[] args) {
-        String token = "MTM2OTE0MTY1MjExOTM1OTUzOA.G_B3SP.HcLCTeuAeVDcnyeEm2KQiTCbMtZtZJtiO2bnDU";
+        String token = EnvUtils.getEnv(BotConstant.BOT_INTRODUCTION_KEY);
 
         JDABuilder.createDefault(token)
                 .enableIntents(
